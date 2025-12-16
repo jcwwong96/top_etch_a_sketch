a25eb426f5a84f2f6bc5479e9d4900d52177ab29
jcwwong96 <236256517+jcwwong96@users.noreply.github.com>
2025-12-16 01:39:22 -0400

    Project Completed 1. added color picker 2. added clear function 3. added rainbow color option 4. added coloring effect toggle button 5. added grid resizing button

--- jscript.js
+++ jscript.js
@@ -1,26 +1,32 @@
 const gridContainer = document.querySelector("#grid_container");
+const gridSizeBtn = document.querySelector("#gridSize");
+const colorPicker = document.querySelector("#colorPicker");
+const dEffect = document.querySelector("#darken");
+const rainbow = document.querySelector("#rainbow");
+const clear = document.querySelector("#clear");
 
-function  createGrid(size){
+
+
+function createGrid(size){
     for (let i = 0; i < size; i++) {
         const grid = document.createElement("div");
         grid.classList.add("grid");
         gridContainer.appendChild(grid);
     }
 }
+let gridSize = 16;
+createGrid((gridSize * gridSize));
 
-const smallGrid = 16 * 16
 
-createGrid(smallGrid)
 
-//only generates light shade
+let color = "";
 function generateRandomRgbColor(){
-    const r = Math.floor(Math.random() * 31) + 200; //number starts from 200 and up to only 230 to avoid near white color
-    const g = Math.floor(Math.random() * 31) + 200;
-    const b = Math.floor(Math.random() * 31) + 200; 
-    const rgb = `rgb(${r}, ${g}, ${b}`;
+    const r = Math.floor(Math.random() * 255); 
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255); 
+    const rgb = `rgb(${r}, ${g}, ${b})`;
     return rgb
 }
-
 /*function changeBackgroundColor(event) {
     event.target.style.backgroundColor = generateRandomRgbColor();
     event.currentTarget.style.backgroundColor = "";
@@ -30,12 +36,102 @@ function generateRandomRgbColor(){
 
 //using mouseover will change the color of the same grid everytime the mouse reenters.
 //this can be solved by either using one color or
-//using mouseenter targeting each child of gridContainer
+//using mouseenter with {once: true} targeting each child of gridContainer
+function drawing(color) {
+    const grids = document.querySelectorAll(".grid"); //this must stay in the drawing function
+    for (const grid of grids) {
+        let newHW = 100 / gridSize;
 
-const grids = document.querySelectorAll(".grid");
+        grid.style.setProperty("width", `${newHW}%`);
+        grid.style.setProperty("height", `${newHW}%`);
 
-for (const grid of grids) {
-    grid.addEventListener("mouseenter", (event) => {
-        event.target.style.backgroundColor = generateRandomRgbColor();
-    }, {once: true})
-};
+        grid.addEventListener("mouseenter", (event) => {
+            if(color == ""){
+                event.target.style.backgroundColor = generateRandomRgbColor();
+            }else {
+                event.target.style.backgroundColor = color;
+            }
+        })
+    };
+}
+drawing(color);
+
+
+
+colorPicker.addEventListener("input", (event) => {
+    //console.log(event.target.value);
+    color = event.target.value;
+    drawing(color);
+})
+
+
+
+rainbow.addEventListener("click", (event) => {
+    color = "";
+    drawing(color);
+})
+
+
+
+/*dEffect.addEventListener("click", (event) => {
+        const grids = document.querySelectorAll(".grid");
+        for (const grid of grids) {
+            let counter = 0.10;
+            grid.addEventListener("mouseenter", (event) => {
+                event.target.style.opacity = counter;
+                console.log(counter);
+                if(counter < 1) {
+                    counter += 0.10;
+                    //each square takes 10 times to be full colored
+                }
+        })
+        }
+    })*/
+let isListenerOn = true;
+function darkening(num){
+    const grids = document.querySelectorAll(".grid");
+    for (const grid of grids) {
+        let counter = num;
+        grid.addEventListener("mouseenter", (event) => {
+            event.target.style.opacity = counter;
+            //console.log(counter);
+            if(counter < 1) {
+                counter += 0.10;
+                //each square takes 10 times to be full colored
+            }
+        })
+        }
+}
+function toggleListener(){
+    if (isListenerOn) {
+        darkening(0.1);
+        isListenerOn = false;
+        //console.log(isListenerOn)
+        dEffect.textContent = "Solid Color"
+    } else if (isListenerOn == false) {
+        darkening(1)
+        isListenerOn = true;
+        //console.log(isListenerOn)
+        dEffect.textContent = "Darkening Effect"
+    }
+}
+dEffect.addEventListener("click", toggleListener);
+
+
+
+gridSizeBtn.addEventListener("click", (event) => {
+    gridSize = prompt("Type your grid size: ", "up to 100");
+    gridContainer.innerHTML = "";
+    createGrid((gridSize * gridSize));
+    //console.log(gridSize * gridSize);
+    drawing(color); 
+})
+
+
+
+clear.addEventListener("click", (event) => {
+    const grids = document.querySelectorAll(".grid");
+    for (const grid of grids) {
+        grid.style.backgroundColor = "white";
+    }
+})
